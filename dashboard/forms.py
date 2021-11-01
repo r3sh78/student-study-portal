ae074e2c0aac0f819c773afc8893d6ebf2d8b0e7
r3sh78 <63780356+r3sh78@users.noreply.github.com>
2021-11-01 22:02:46 +0530

    code push

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -23,3 +23,18 @@ class TodoForm(forms.Form):
     class Meta:
         model = Todo
         fields = ['title','is_finished']
+
+class ConversionForm(forms.Form):
+    CHOICES = [('length'),('Length'),('mass','Mass')]
+    measurement = forms.ChoiceField(choices = CHOICES,widget=forms.RadioSelect)
+
+class ConversionLengthForm(forms.Form):
+    CHOICES = [('yard','Yard'),('foot','Foot')]
+    input = forms.CharField(required=False,label=False,widget=forms.TextInput
+    (attrs = {'type':'number','placeholder':'Enter the Number'}))
+    measure1 = forms.CharField(
+        lable = '',widget = forms.Select(choices = CHOICES)
+    )
+    measure2 = forms.CharField(
+        lable='', widget=forms.Select(choices=CHOICES)
+    )
