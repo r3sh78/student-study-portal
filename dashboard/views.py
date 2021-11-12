957e0820676dfcbc318cd3e9e88869833859ab0a
r3sh78 <63780356+rushabhpanbude@users.noreply.github.com>
2021-11-12 13:03:32 +0530

    Redirect update

--- dashboard/views.py
+++ dashboard/views.py
@@ -320,18 +320,18 @@ def register(request):
 def profile(request):
     homeworks = Homework.objects.filter(is_finished=False,user=request.user)
     todos = Todo.objects.filter(is_finished=False,user=request.user)
-    if delete_notes(homework) == 0:
+    if len(homeworks) == 0:
         homework_done = True
     else:
         homework_done = False
-    if len(todo) == 0:
+    if len(todos) == 0:
         todos_done = True
-     else:
+    else:
         todos_done = False
     context = {
-    'homeworks': homework,
-    'todos_done':todos,
-    'homeworks_done': homework_done,
-    'todos_done': todos_done
+        'homeworks': homeworks,
+        'todos':todos,
+        'homeworks_done': homework_done,
+        'todos_done': todos_done
     }
     return render(request,"dashboard/profile.html",context)
